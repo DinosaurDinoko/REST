c8bc934041422f2b1625097bc536e1623c90487f
DinosaurDinoko <mega.lelya1235@gmail.com>
2023-10-24 21:36:03 +0700

    admin.

--- DjangoDesign/catalog/admin.py
+++ DjangoDesign/catalog/admin.py
@@ -1,3 +1,11 @@
+# импорт модуля административной панели Django.
 from django.contrib import admin
+# мпорт моделей Design и Category из приложения.
+from .models import Design, Category
 
-# Register your models here.
+# регистрация модели Design в административной панели.
+# Это позволяет администраторам сайта управлять объектами этой модели через административный интерфейс.
+admin.site.register(Design)
+
+# регистрация модели Category в административной панели.
+admin.site.register(Category)
